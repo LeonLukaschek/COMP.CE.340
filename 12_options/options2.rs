d8b0ac9b1341776cb5583c249fba60bf007814e3
Leon Lukaschek <leon.lukaschek@tuni.fi>
2024-09-12 14:30:19 +0300

    options2.rs done

--- 12_options/options2.rs
+++ 12_options/options2.rs
@@ -26,10 +26,6 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: Make this a while-let statement. Remember that `Vec::pop()`
-        // adds another layer of `Option`. You can do nested pattern matching
-        // in if-let and while-let statements.
-
 
         while let Some(integer) = optional_integers.pop(){
             if let Some(inner) = integer{
@@ -39,10 +35,6 @@ mod tests {
             
         }
 
-        // integer = optional_integers.pop() {
-        //     assert_eq!(integer, cursor);
-        //     cursor -= 1;
-        // }
 
         assert_eq!(cursor, 0);
     }
